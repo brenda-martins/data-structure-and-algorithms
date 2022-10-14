e4cb43f528b0284aa7f669b1aff34bbaf2451ab3
Brenda Martins <brendamichele16@hotmail.com>
2022-10-14 15:17:50 -0300

    Improvements in class List

--- src/brenda/datastruct/sequentialList/List.java
+++ src/brenda/datastruct/sequentialList/List.java
@@ -1,18 +1,18 @@
 package brenda.datastruct.sequentialList;
 
 public class List<T> {
-    T data[];
-    int totalOfElements;
+    private T elements[];
+    private int totalOfElements;
 
 
     public List(final int aSize){
-        this.data = (T[]) new Object[aSize];
+        this.elements = (T[]) new Object[aSize];
         this.totalOfElements = 0;
     }
 
     public void list(){
         for(int i = 0; i < this.totalOfElements; i++){
-            System.out.println(data[i]);
+            System.out.println(elements[i]);
         }
     }
 
@@ -21,11 +21,11 @@ public class List<T> {
     }
 
     public boolean insert(T element, int position){
-        if(position >= 0 && position < this.data.length){
+        if(position >= 0 && position < this.elements.length){
             for (int i = this.totalOfElements; i > position; i--){
-                this.data[i] = this.data[i-1];
+                this.elements[i] = this.elements[i-1];
             }
-            this.data[position] = element;
+            this.elements[position] = element;
             this.totalOfElements++;
             return true;
         }
@@ -33,8 +33,8 @@ public class List<T> {
     }
 
     public boolean insert(T element){
-        if(this.totalOfElements < this.data.length){
-            this.data[this.totalOfElements] = element;
+        if(this.totalOfElements < this.elements.length){
+            this.elements[this.totalOfElements] = element;
             this.totalOfElements++;
             return true;
         }
@@ -44,11 +44,19 @@ public class List<T> {
     public T searchElement(T element){
         int i = 0;
         while(i < this.totalOfElements){
-            if(element == this.data[i]) return this.data[i];
+            if(element == this.elements[i]) return this.elements[i];
             else i++;
         }
-
         return null;
     }
+
+//    public void sort(){
+//        for(int i = this.data.length - 1; i > 0; i++){
+//            if(this.data[i] < this.data[i-1]){
+//
+//            }
+//        }
+//    }
+
 
 }
