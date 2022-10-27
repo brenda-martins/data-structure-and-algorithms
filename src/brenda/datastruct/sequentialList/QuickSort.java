6794aaa092b24a4d0121cfde32d2cde43efb594e
Brenda Martins <brendamichele16@hotmail.com>
2022-10-26 22:32:00 -0300

    Creating quick sort algorithm

---
+++ src/brenda/datastruct/sequentialList/QuickSort.java
@@ -0,0 +1,23 @@
+package brenda.datastruct.sequentialList;
+
+public class QuickSort {
+
+    private static int[] vector = new int[10];
+
+    public static void main(String[] args) {
+
+
+        for(int i = 0; i < vector.length; i++){
+            vector[i] = (int) (Math.random() * vector.length);
+        }
+
+        print("Vector: ");
+    }
+
+    private static void print(String message){
+        System.out.println(message);
+        for(int i = 0; i < vector.length; i++){
+            System.out.print(vector[i] + " ");
+        }
+    }
+}
